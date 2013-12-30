a67757a7ab160a2bfc062cf2a75573fb2b46b51a
chickenrainteam <chickenrainteam@users.noreply.github.com>
2013-12-30 16:06:17 +0700

    Update routes.php
    Fix the empty string

--- Config/routes.php
+++ Config/routes.php
@@ -2,4 +2,3 @@
 	Router::connect('/my_upload', array('plugin'=>'my_upload', 'controller' => 'fileuploads', 'action' => 'index'));
 	Router::connect('/my_upload/upload', array('plugin'=>'my_upload', 'controller' => 'fileuploads', 'action' => 'add'));
 	Router::connect('/my_upload/multi_upload', array('plugin'=>'my_upload', 'controller' => 'fileuploads', 'action' => 'multi_upload'));
-?>
